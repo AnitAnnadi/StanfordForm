62eb37937d7af6a1717e6ef6e08bb5d7ae3961fc
cyril <97067553+cyriljoby@users.noreply.github.com>
2023-04-03 00:56:47 -0700

    modelss created

---
+++ models/Question.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const Question = new mongoose.Schema(
+  {
+    StudentResponse: {
+      type: mongoose.Types.ObjectId,
+      required: [true, 'Please provide UUID'],
+    },
+    Question: {
+      required: [true, 'Please provide Question'],
+    },
+    Answer: {
+      type: String,
+      required: [true, 'Please provide answer'],
+    }
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model('Question', Question)
