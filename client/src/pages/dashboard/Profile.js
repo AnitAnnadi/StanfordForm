22cb3df2105ca59b483d8a9f86c7b381160c1c44
Anit Annadi <72627877+AnitAnnadi@users.noreply.github.com>
2023-04-07 21:18:15 -0700

    Update Profile.js

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,62 +1,70 @@
-import { useState } from 'react'
-import { FormRow, Alert } from '../../components'
-import { useAppContext } from '../../context/appContext'
-import Wrapper from '../../assets/wrappers/DashboardFormPage'
-import { v4 as uuid } from 'uuid';
+import { useState } from "react";
+import { FormRow, Alert } from "../../components";
+import { useAppContext } from "../../context/appContext";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { v4 as uuid } from "uuid";
 
 const Profile = () => {
-  const { user, userLocations, showAlert, displayAlert, updateUser, isLoading } =
-    useAppContext()
+  const {
+    user,
+    userLocations,
+    showAlert,
+    displayAlert,
+    updateUser,
+    isLoading,
+  } = useAppContext();
 
-  const [name, setName] = useState(user?.name)
-  const [email, setEmail] = useState(user?.email)
+  const [name, setName] = useState(user?.name);
+  const [email, setEmail] = useState(user?.email);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (!name || !email) {
-      displayAlert()
-      return
+      displayAlert();
+      return;
     }
-    updateUser({ name, email })
-  }
+    updateUser({ name, email });
+  };
 
   return (
     <Wrapper>
-      <form className='form' onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit}>
         <h3>profile</h3>
         {showAlert && <Alert />}
-        <div className='form-center'>
+        <div className="form-center">
           <FormRow
-            type='text'
-            name='name'
+            type="text"
+            name="name"
             value={name}
             handleChange={(e) => setName(e.target.value)}
           />
           <FormRow
-            type='email'
-            name='email'
+            type="email"
+            name="email"
             value={email}
             handleChange={(e) => setEmail(e.target.value)}
           />
-          <hr/>
-          <h4>locations</h4>
-          <ul className="scrollable-locations">
-            {userLocations.map(location => {
-                return (
-                  <li
-                    key={location.index}
-                  >{location.school} - {location.city}, {location.state}
-                  </li>
-                );
-              })}
-          </ul>
-          <button className='btn btn-block' type='submit' disabled={isLoading}>
-            {isLoading ? 'Please Wait...' : 'save changes'}
+          {/* <hr /> */}
+          <label
+            className="form-label"
+            style={{ fontSize: "1rem", marginBottom: 0 }}
+          >
+            locations
+          </label>
+          {userLocations.map((location) => {
+            return (
+              <p key={location.index} className="location">
+                {location.school} - {location.city}, {location.state}
+              </p>
+            );
+          })}
+          <button className="btn btn-block" type="submit" disabled={isLoading}>
+            {isLoading ? "Please Wait..." : "save changes"}
           </button>
         </div>
       </form>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
