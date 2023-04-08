049673302109590aa70f9e25165b63032b9cbab1
Anit Annadi <72627877+AnitAnnadi@users.noreply.github.com>
2023-04-07 21:18:44 -0700

    Update Form.js

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -15,61 +15,98 @@ const Form = () => {
     updateLocation,
     isLoading,
     enterCode,
-    submitForm
+    submitForm,
   } = useAppContext();
-  let tobacco=[{"question":"I know that the “vapor” that comes out of a vaping device is","answers":["mostly harmless water","some aerosolized chemicals","mosstly aerosolized chemicals"]},{"question":"How much waste/garbage do you believe tobacco/vaping products produce? ","answers":["not at all","a little", "a moderate amount", "a lot"]}]
-  let cannabis=[{"question":"I know that the “vapor” that comes out of a cannabis device is","answers":["mostly harmless water","some aerosolized chemicals","mosstly aerosolized chemicals"]},{"question":"How much waste/garbage do you believe cannabis products produce? ","answers":["not at all","a little", "a moderate amount", "a lot"]}]
+  let tobacco = [
+    {
+      question: "I know that the “vapor” that comes out of a vaping device is",
+      answers: [
+        "mostly harmless water",
+        "some aerosolized chemicals",
+        "mosstly aerosolized chemicals",
+      ],
+    },
+    {
+      question:
+        "How much waste/garbage do you believe tobacco/vaping products produce? ",
+      answers: ["not at all", "a little", "a moderate amount", "a lot"],
+    },
+  ];
+  let cannabis = [
+    {
+      question:
+        "I know that the “vapor” that comes out of a cannabis device is",
+      answers: [
+        "mostly harmless water",
+        "some aerosolized chemicals",
+        "mosstly aerosolized chemicals",
+      ],
+    },
+    {
+      question:
+        "How much waste/garbage do you believe cannabis products produce? ",
+      answers: ["not at all", "a little", "a moderate amount", "a lot"],
+    },
+  ];
   const navigate = useNavigate();
   let location = useLocation();
   let info = location.state;
-  let selected=[]
-  let names=[]
+  let selected = [];
+  let names = [];
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(names)
-    names.map((name)=>{
-        let checks=document.getElementsByName(name)
-        for (var check of checks)
-        {
+    console.log(names);
+    names.map((name) => {
+      let checks = document.getElementsByName(name);
+      for (var check of checks) {
         if (check.checked) {
-            selected.push(check.value)
+          selected.push(check.value);
         }
-        }
-    })
-    console.log(selected)
-    submitForm(names,selected,localStorage.getItem('code'),info["grade"],info["when"],info["form"])
+      }
+    });
+    console.log(selected);
+    submitForm(
+      names,
+      selected,
+      localStorage.getItem("code"),
+      info["grade"],
+      info["when"],
+      info["form"]
+    );
 
-    navigate("/success", {
-      });
-    
+    navigate("/success", {});
   };
 
-  return(
+  return (
     <form className="form" onSubmit={handleSubmit}>
-      <h4>{info["form"]}</h4>
       {showAlert && <Alert />}
-      {console.log(eval(info["form"]))}
-        {eval(info["form"]).map((element, index) => (
-          <div>
-            {names.push(element["question"])}
-            <h4>{element["question"]}</h4>
-            {element["answers"].map((answer)=>{
-              return(
-              <div>
-                <input type="radio" value={answer} name={element["question"]} /> {answer}
-              </div>)
-            })}
+      <h3>{info["form"]}</h3>
+      {eval(info["form"]).map((element, index) => (
+        <div>
+          <div style={{ display: "flex", columnGap: "0.35rem" }}>
+            <p>{names.push(element["question"])}.</p>
+            <p>{element["question"]}</p>
           </div>
-          ))}
-        <button
-          className="btn btn-block"
-          type="submit"
-          onSubmit={(e) => handleSubmit(e.target.value)}
-          style={{ marginTop: "1.38rem" }}
-        >
-          submit
-        </button>
-      </form>
-  )
+          {element["answers"].map((answer) => {
+            return (
+              <label class="container">
+                <span>{answer}</span>
+                <input type="radio" value={answer} name={element["question"]} />
+                <span class="checkmark"></span>
+              </label>
+            );
+          })}
+        </div>
+      ))}
+      <button
+        className="btn btn-block"
+        type="submit"
+        onSubmit={(e) => handleSubmit(e.target.value)}
+        style={{ marginTop: "1.38rem" }}
+      >
+        submit
+      </button>
+    </form>
+  );
 };
 export default Form;
