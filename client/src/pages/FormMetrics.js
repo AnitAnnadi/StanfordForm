7768a149b834ac20c0440e70c9e7dc587f838c05
Anit Annadi <72627877+AnitAnnadi@users.noreply.github.com>
2023-04-23 13:30:57 -0700

    Update FormMetrics.js

--- client/src/pages/FormMetrics.js
+++ client/src/pages/FormMetrics.js
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from "react";
-import {Chart as ChartJS, ArcElement, Tooltip, Legend, Title} from "chart.js";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from "chart.js";
 import { Pie, Doughnut } from "react-chartjs-2";
-import { useParams, useNavigate, useLocation } from 'react-router-dom';
-import {Loading} from "../components";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
+import { Loading } from "../components";
 import Wrapper from "../assets/wrappers/DashboardFormPage";
 import ResponseGroupInfo from "../components/ResponseGroupInfo";
-import {FaChalkboardTeacher, FaRegCalendarAlt} from "react-icons/fa";
-import {AiOutlineForm, AiOutlineNumber} from "react-icons/ai";
-import {TbListNumbers, TbNumbers} from "react-icons/tb";
+import { FaChalkboardTeacher, FaRegCalendarAlt } from "react-icons/fa";
+import { AiOutlineForm, AiOutlineNumber } from "react-icons/ai";
+import { TbListNumbers, TbNumbers } from "react-icons/tb";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const dynamicColors = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    return "rgb(" + r + "," + g + "," + b + ")";
-}
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return "rgb(" + r + "," + g + "," + b + ")";
+};
 
 const FormMetrics = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -44,75 +44,86 @@ const FormMetrics = () => {
         setIsLoading(false);
       })
       .catch((error) => console.error(error));
-
   }, [formCode, location.search]);
 
   if (isLoading) return <Loading center />;
 
   return (
-    <Wrapper>
+    <Wrapper style={{ maxWidth: "800px" }}>
       <header>
-        <div className='info'>
+        <div className="info">
           <h3>{school.school}</h3>
-          <h4>{school.city}, {school.state}</h4>
+          <h4>
+            {school.city}, {school.state}
+          </h4>
         </div>
       </header>
-      <div className='content'>
-        <div className='content-center'>
-            <ResponseGroupInfo icon={<FaChalkboardTeacher />} text={teacher.name} />
-            <ResponseGroupInfo icon={<AiOutlineNumber />} text={formCode} />
-            <ResponseGroupInfo icon={<TbListNumbers />} text=
-            {responseType?.period ? 'Period ' + responseType.period:
-              'No specified period'}
-            />
-            <ResponseGroupInfo icon={<TbNumbers />} text={'Grade ' + responseType.grade} />
-            <ResponseGroupInfo icon={<AiOutlineForm />} text={responseType.formType} />
-            <ResponseGroupInfo icon={<FaRegCalendarAlt />} text={responseType.when} />
+      <div className="content">
+        <div className="content-center">
+          <ResponseGroupInfo
+            icon={<FaChalkboardTeacher />}
+            text={teacher.name}
+          />
+          <ResponseGroupInfo icon={<AiOutlineNumber />} text={formCode} />
+          <ResponseGroupInfo
+            icon={<TbListNumbers />}
+            text={
+              responseType?.period
+                ? "Period " + responseType.period
+                : "No specified period"
+            }
+          />
+          <ResponseGroupInfo
+            icon={<TbNumbers />}
+            text={"Grade " + responseType.grade}
+          />
+          <ResponseGroupInfo
+            icon={<AiOutlineForm />}
+            text={responseType.formType}
+          />
+          <ResponseGroupInfo
+            icon={<FaRegCalendarAlt />}
+            text={responseType.when}
+          />
         </div>
       </div>
-      {/*<header>*/}
-      {/*  <h3>{`Form metrics`}</h3>*/}
-      {/*  <h4>{`${school.school}`}</h4>*/}
-      {/*  <h4>{`${school.city}, ${school.state}`}</h4>*/}
-      {/*  <h4>{`${teacher.name}'s class`}</h4>*/}
-      {/*  <h5>{`Form code: ${formCode}`}</h5>*/}
-      {/*  <h5>{`Period: ${responseType.period ? responseType.period: "unspecified"}`}</h5>*/}
-      {/*  <h5>{`Grade: ${responseType.grade}`}</h5>*/}
-      {/*  <h5>{`Form type: ${responseType.formType}`}</h5>*/}
-      {/*  <h5>{`when: ${responseType.when}`}</h5>*/}
-      {/*</header>*/}
-
-      <div className='content'>
-        <div className='content-center'>
+      <div className="content">
+        <div className="content-center">
           {Object.keys(questionsToAnswers).map((question, index) => (
             <div key={index}>
-              <h5 style={{padding: "1rem 1.5rem"}}>{question}</h5>
+              <h5 style={{ padding: "1rem 0" }}>{question}</h5>
               <div className="chartCanvas">
                 <Doughnut
-                  data={
-                    {
-                      labels: Object.keys(questionsToAnswers[question]),
-                      datasets: [
-                        {
-                          label: "How many gave this answer",
-                          data: Object.values(questionsToAnswers[question]),
-                          backgroundColor: Object.keys(questionsToAnswers[question]).map(() => dynamicColors()),
-                          borderWidth: 1
-                        }
-                      ]
-                    }
-                  }
-                  options={
-                    {
-                      responsive: true,
-                      maintainAspectRatio: true,
-                      plugins: {
-                        legend: {
-                          position: 'right',
-                        }
-                      }
-                    }
-                  }
+                  data={{
+                    labels: Object.keys(questionsToAnswers[question]),
+                    datasets: [
+                      {
+                        label: "How many gave this answer",
+                        data: Object.values(questionsToAnswers[question]),
+                        // backgroundColor: Object.keys(
+                        //   questionsToAnswers[question]
+                        // ).map(() => dynamicColors()),
+                        backgroundColor: [
+                          "#d0203f",
+                          "#A2C3DB",
+                          "#8871A0",
+                          "#8AAF22",
+                          "#DCB12D",
+                          "#3F9F9F",
+                        ],
+                        borderWidth: 1,
+                      },
+                    ],
+                  }}
+                  options={{
+                    responsive: true,
+                    maintainAspectRatio: true,
+                    plugins: {
+                      legend: {
+                        position: "right",
+                      },
+                    },
+                  }}
                 />
               </div>
               {/*{Object.keys(questionsToAnswers[question]).map((answer, index) => (*/}
@@ -126,11 +137,12 @@ const FormMetrics = () => {
         </div>
       </div>
       <footer>
-        <button className='btn btn-block' onClick={() => navigate(-1)}>Go back</button>
+        <button className="btn btn-block" onClick={() => navigate(-1)}>
+          Go back
+        </button>
       </footer>
     </Wrapper>
   );
 };
 
 export default FormMetrics;
-
