f626258a8bd5ed58247c0a349c5a4130792be5a2
cyril <97067553+cyriljoby@users.noreply.github.com>
2023-04-25 21:18:39 -0700

    rm home from admin navbar

--- client/src/utils/links.js
+++ client/src/utils/links.js
@@ -2,10 +2,31 @@ import { FaUser } from "react-icons/fa";
 import { ImStatsBars } from "react-icons/im";
 import { MdDashboard } from "react-icons/md";
 
-const links = [
-  { id: 1, text: "home", path: "/", icon: <MdDashboard /> },
-  { id: 2, text: "metrics", path: "metrics", icon: <ImStatsBars /> },
-  { id: 3, text: "profile", path: "profile", icon: <FaUser /> },
-];
+const user=JSON.parse((localStorage.getItem("user")))
+let adminroles = ["Site Admin", "District Admin", "County Admin", "State Admin", "Standford Staff"];
+let adminbool=false
+let links=[]
+adminroles.map((role=>{
+  if (role==user["role"]){
+    adminbool=true
+  }
+}))
+
+if (adminbool===true){
+  links = [
+    // { id: 1, text: "home", path: "/", icon: <MdDashboard /> },
+    { id: 2, text: "metrics", path: "metrics", icon: <ImStatsBars /> },
+    { id: 3, text: "profile", path: "profile", icon: <FaUser /> },
+  ];
+}
+else{
+  links = [
+    { id: 1, text: "home", path: "/", icon: <MdDashboard /> },
+    { id: 2, text: "metrics", path: "metrics", icon: <ImStatsBars /> },
+    { id: 3, text: "profile", path: "profile", icon: <FaUser /> },
+  ];
+}
+
+console.log(adminbool)
 
 export default links;
