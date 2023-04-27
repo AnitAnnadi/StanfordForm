3f66e45d32dd747c4cfc9c95a1c6d67d6a4bcf54
cyril <97067553+cyriljoby@users.noreply.github.com>
2023-04-26 20:20:45 -0700

    navbar fix

--- client/src/components/NavLinks.js
+++ client/src/components/NavLinks.js
@@ -1,7 +1,36 @@
 import links from '../utils/links';
 import { NavLink } from 'react-router-dom';
+import { useEffect } from 'react';
+import { FaUser } from "react-icons/fa";
+import { ImStatsBars } from "react-icons/im";
+import { MdDashboard } from "react-icons/md";
 
 const NavLinks = ({ toggleSidebar }) => {
+  const user=JSON.parse((localStorage.getItem("user")))
+  let adminroles = ["Site Admin", "District Admin", "County Admin", "State Admin", "Standford Staff"];
+  let adminbool=false
+  let links=[]
+  adminroles.map((role=>{
+    if (role==user?.["role"]){
+      adminbool=true
+    }
+  }))
+
+  if (adminbool===true){
+    links = [
+      // { id: 1, text: "home", path: "/", icon: <MdDashboard /> },
+      { id: 2, text: "metrics", path: "metrics", icon: <ImStatsBars /> },
+      { id: 3, text: "profile", path: "profile", icon: <FaUser /> },
+    ];
+  }
+  else{
+    links = [
+      { id: 1, text: "home", path: "/", icon: <MdDashboard /> },
+      { id: 2, text: "metrics", path: "metrics", icon: <ImStatsBars /> },
+      { id: 3, text: "profile", path: "profile", icon: <FaUser /> },
+    ];
+  }
+
   return (
     <div className='nav-links'>
       {links.map((link) => {
--- client/src/utils/links.js
+++ client/src/utils/links.js
@@ -1,32 +1,13 @@
 import { FaUser } from "react-icons/fa";
 import { ImStatsBars } from "react-icons/im";
 import { MdDashboard } from "react-icons/md";
-
-const user=JSON.parse((localStorage.getItem("user")))
-let adminroles = ["Site Admin", "District Admin", "County Admin", "State Admin", "Standford Staff"];
-let adminbool=false
-let links=[]
-adminroles.map((role=>{
-  if (role==user?.["role"]){
-    adminbool=true
-  }
-}))
-
-if (adminbool===true){
-  links = [
+// do not use anymore
+let  links = [
     // { id: 1, text: "home", path: "/", icon: <MdDashboard /> },
     { id: 2, text: "metrics", path: "metrics", icon: <ImStatsBars /> },
     { id: 3, text: "profile", path: "profile", icon: <FaUser /> },
   ];
-}
-else{
-  links = [
-    { id: 1, text: "home", path: "/", icon: <MdDashboard /> },
-    { id: 2, text: "metrics", path: "metrics", icon: <ImStatsBars /> },
-    { id: 3, text: "profile", path: "profile", icon: <FaUser /> },
-  ];
-}
 
-console.log(adminbool)
+
 
 export default links;
