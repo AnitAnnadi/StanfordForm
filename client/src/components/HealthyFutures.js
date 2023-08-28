5eb456c9eebcfa7bef2681d80017fe86142814b3
Anit Annadi <72627877+AnitAnnadi@users.noreply.github.com>
2023-08-27 23:36:49 -0700

    Update HealthyFutures.js

--- client/src/components/HealthyFutures.js
+++ client/src/components/HealthyFutures.js
@@ -1,5 +1,5 @@
-import { useAppContext } from '../context/appContext';
-import { useEffect } from 'react';
+import { useAppContext } from "../context/appContext";
+import { useEffect } from "react";
 
 const HealthyFeatures = () => {
   const {
@@ -9,37 +9,42 @@ const HealthyFeatures = () => {
     displayAlert,
     user,
     healthyFuturesListCannabis,
-    healthyFuturesListTobacco
+    healthyFuturesListTobacco,
   } = useAppContext();
 
   useEffect(() => {
     getHealthyFutures(user._id);
   }, []);
 
-  const listItemStyle = {
-    marginLeft: '20px', // Add the desired indentation value
-  };
-
   return (
     <div>
       <div>
-        <p>Healthy Futures: Cannabis</p>
-        {healthyFuturesListCannabis.length>0?
-        healthyFuturesListCannabis.map((response) => (
-          <div key={response._id} style={listItemStyle}>
-            {response.name}
-          </div>
-        )):<>There are no current responses to this form</>}
+        <p>Students Who Completed Healthy Futures: Tobacco/Nicotine/Vaping</p>
+        {healthyFuturesListTobacco.length > 0 ? (
+          healthyFuturesListTobacco.map((response) => (
+            <p className="location" key={response._id}>
+              {response.name}
+            </p>
+          ))
+        ) : (
+          <p className="location">
+            There are no current students who have completed this course
+          </p>
+        )}
       </div>
       <div>
-
-        <p>Healthy Futures: Tobacco/Nicotine/Vaping</p>
-        {healthyFuturesListTobacco.length>0?
-        healthyFuturesListTobacco.map((response) => (
-          <div key={response._id} style={listItemStyle}>
-            {response.name}
-          </div>
-        )):<>There are no current responses to this form</>}
+        <p>Students Who Completed Healthy Futures: Cannabis</p>
+        {healthyFuturesListCannabis.length > 0 ? (
+          healthyFuturesListCannabis.map((response) => (
+            <div className="location" key={response._id}>
+              {response.name}
+            </div>
+          ))
+        ) : (
+          <p className="location">
+            There are no current students who have completed this course
+          </p>
+        )}
       </div>
     </div>
   );
