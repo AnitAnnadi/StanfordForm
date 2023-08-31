5bbf1dcf0cbf6f7758b344ceac109d68d0652073
Anit Annadi <72627877+AnitAnnadi@users.noreply.github.com>
2023-08-30 17:27:52 -0700

    Update TwoFactorSent.js

--- client/src/pages/TwoFactorSent.js
+++ client/src/pages/TwoFactorSent.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useAppContext } from '../context/appContext';
-import { Alert, Loading } from '../components';
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useAppContext } from "../context/appContext";
+import { Alert, Loading } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
 
 const TwoFactorSent = () => {
   const location = useLocation();
@@ -14,7 +15,7 @@ const TwoFactorSent = () => {
 
     try {
       setIsResending(true);
-      console.log(location.state)
+      console.log(location.state);
       await resendEmail(currentUser.email);
       setIsResending(false);
     } catch (error) {
@@ -23,14 +24,27 @@ const TwoFactorSent = () => {
   };
 
   return (
-    <div>
-      <p>Complete Two Factor Authentication from the link sent to your email.</p>
-      <form onSubmit={onSubmit}>
-        <button type="submit" className="btn btn-block" disabled={isLoading || isResending}>
-          {isResending ? 'Resending...' : 'Resend Email'}
-        </button>
-      </form>
-      {showAlert && <Alert />}
+    <div
+      className="full-page"
+      style={{ display: "flex", alignItems: "center", padding: "0 1.5rem" }}
+    >
+      <Wrapper>
+        <div className="form">
+          <h3>
+            Complete Two Factor Authentication from the link sent to your email.
+          </h3>
+          <form onSubmit={onSubmit}>
+            {showAlert && <Alert />}
+            <button
+              type="submit"
+              className="btn btn-block"
+              disabled={isLoading || isResending}
+            >
+              {isResending ? "Resending..." : "Resend Email"}
+            </button>
+          </form>
+        </div>
+      </Wrapper>
     </div>
   );
 };
