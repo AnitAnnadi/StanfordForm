ed21be225d5ba282f4698938611a21177182574c
cyril <97067553+cyriljoby@users.noreply.github.com>
2023-09-03 14:30:41 -0700

    rmove hashing

--- models/Pending.js
+++ models/Pending.js
@@ -41,11 +41,7 @@ const PendingSchema = new mongoose.Schema({
 
 })
 
-PendingSchema.pre('save', async function () {
-    if (!this.isModified('password')) return
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-  })
+
 
 
 
