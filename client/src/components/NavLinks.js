7458ce501854b487a49f7bb29cfef0e862b356de
Anit Annadi <72627877+AnitAnnadi@users.noreply.github.com>
2023-09-26 18:02:53 -0700

    Update NavLinks.js

--- client/src/components/NavLinks.js
+++ client/src/components/NavLinks.js
@@ -1,43 +1,54 @@
-import links from '../utils/links';
-import { NavLink } from 'react-router-dom';
-import { useEffect } from 'react';
-import { FaUser } from "react-icons/fa";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
+import { useEffect } from "react";
+import { FaUser, FaSchool } from "react-icons/fa";
 import { ImStatsBars } from "react-icons/im";
 import { MdDashboard } from "react-icons/md";
 
 const NavLinks = ({ toggleSidebar }) => {
-  const user=JSON.parse((localStorage.getItem("user")))
-  let adminroles = ["Site Admin", "District Admin", "County Admin", "State Admin", "Stanford Staff"];
-  let adminbool=false
-  let links=[]
-  adminroles.map((role=>{
-    if (role==user?.["role"]){
-      adminbool=true
+  const user = JSON.parse(localStorage.getItem("user"));
+  let adminroles = [
+    "Site Admin",
+    "District Admin",
+    "County Admin",
+    "State Admin",
+    "Stanford Staff",
+  ];
+  let adminbool = false;
+  let links = [];
+  adminroles.map((role) => {
+    if (role == user?.["role"]) {
+      adminbool = true;
     }
-  }))
+  });
 
-
-
-  if (adminbool===true && !user?.adminTeacher){
-    links.push({ id: 2, text: "metrics", path: "metrics", icon: <ImStatsBars /> })
-    links.push ({ id: 4, text: "profile", path: "profile", icon: <FaUser /> })
-    
-  }
-  else{
+  if (adminbool === true && !user?.adminTeacher) {
+    links.push({
+      id: 2,
+      text: "metrics",
+      path: "metrics",
+      icon: <ImStatsBars />,
+    });
+    links.push({ id: 4, text: "profile", path: "profile", icon: <FaUser /> });
+  } else {
     links = [
       { id: 1, text: "home", path: "/", icon: <MdDashboard /> },
       { id: 2, text: "metrics", path: "metrics", icon: <ImStatsBars /> },
       { id: 4, text: "profile", path: "profile", icon: <FaUser /> },
     ];
-  
   }
 
-  if (user.role=="Stanford Staff"){
-    links.push({ id: 3, text: "Location Requests", path: "/locationRequests", icon: <MdDashboard /> },)
+  if (user.role == "Stanford Staff") {
+    links.push({
+      id: 3,
+      text: "Location Requests",
+      path: "/locationRequests",
+      icon: <FaSchool />,
+    });
   }
 
   return (
-    <div className='nav-links'>
+    <div className="nav-links">
       {links.map((link) => {
         const { text, path, id, icon } = link;
 
@@ -47,11 +58,11 @@ const NavLinks = ({ toggleSidebar }) => {
             key={id}
             onClick={toggleSidebar}
             className={({ isActive }) =>
-              isActive ? 'nav-link active' : 'nav-link'
+              isActive ? "nav-link active" : "nav-link"
             }
             end
           >
-            <span className='icon'>{icon}</span>
+            <span className="icon">{icon}</span>
             {text}
           </NavLink>
         );
