bd8cdd495bb74e2af25a367bb371e0d9b0c0368e
Anit Annadi <72627877+AnitAnnadi@users.noreply.github.com>
2023-09-26 18:07:58 -0700

    Update CreateLocPopup.js

--- client/src/components/CreateLocPopup.js
+++ client/src/components/CreateLocPopup.js
@@ -1,15 +1,22 @@
 import Popup from "reactjs-popup";
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../context/appContext";
 
-const CreateLocPopup = ({setDisplay}) => {
+const CreateLocPopup = ({ setDisplay }) => {
   const navigate = useNavigate();
   const { handleChange } = useAppContext();
 
   return (
     <div className="welcome">
-      <Popup className="welcome" disableBackdropClick={true} backdrop="static" open={true} modal nested>
+      <Popup
+        className="welcome"
+        disableBackdropClick={true}
+        backdrop="static"
+        open={true}
+        modal
+        nested
+      >
         <div
           className="modal"
           style={{
@@ -33,36 +40,40 @@ const CreateLocPopup = ({setDisplay}) => {
             >
               <li>Make sure the information you input is correct</li>
               <li>Write an appropriate and correctly formatted school name</li>
-              <li>Make sure that your location is not already listed (which may have been listed slightly differently)</li>
+              <li>
+                Make sure that your location is not already listed (which may
+                have been listed slightly differently)
+              </li>
             </ul>
-            <strong>Return</strong> to go back to existing locations if you are unsure about adding one, or <strong>Continue</strong> to add a new location.
+            <strong>Return</strong> to go back to existing locations if you are
+            unsure about adding one, or <strong>Continue</strong> to add a new
+            location.
           </div>
           <div className="modal-btn-container">
-              <button
-                className="btn btn-block submit-btn"
-                onClick={() => {
-                  setDisplay(false);
-                }}
-                style={{ margin: "0.5rem 0" }}
-              >
-                Return
-              </button>
-              <button
-                className="btn btn-block modal-clear-btn"
-                onClick={() => {
-                  handleChange({ name: "pendingApproval", value: false });
-                  setTimeout(() => {
-                    navigate("/createLoc");
-                  }, 500);
-                }}
-              >
-                Continue
-              </button>
-            </div>
+            <button
+              className="btn btn-block submit-btn"
+              onClick={() => {
+                setDisplay(false);
+              }}
+              style={{ margin: "0.5rem 0", cursor: "pointer" }}
+            >
+              Return
+            </button>
+            <button
+              className="btn btn-block modal-clear-btn"
+              onClick={() => {
+                handleChange({ name: "pendingApproval", value: false });
+                setTimeout(() => {
+                  navigate("/createLoc");
+                }, 500);
+              }}
+            >
+              Continue
+            </button>
+          </div>
         </div>
       </Popup>
     </div>
   );
 };
 export default CreateLocPopup;
-
