d5ae2eee78390d679846ae72fb5d94a101c4eaea
cyriljoby <cyriljoby@gmail.com>
2024-08-16 13:14:49 -0700

    temporary selectLoc fix. fix later. changes in home,metrics and register. '

--- client/src/pages/dashboard/Home.js
+++ client/src/pages/dashboard/Home.js
@@ -48,35 +48,35 @@ const Home = () => {
             navigate("/pendingLocation");
           }, 2000);
         }
-        else if (userLocations.length >= 1) {
-          successAlert("Redirecting to select school location");
-          setTimeout(() => {
-            navigate("/selectLoc", {
-              state: { adminTeacher: false, selectSchool: true, fromProfile: false }
-            });
-          }, 2000);
-          return;
-        }
-        else if (userLocations.length === 0 && pendingLocations.length==0) {
-          if (user.adminTeacher && (user.role === 'Stanford Staff' || user.role === "Site Admin")) {
-            successAlert("Redirecting to select school location");
-            setTimeout(() => {
-              navigate("/selectLoc", {
-                state: { adminTeacher: false, selectSchool: true, fromProfile: false }
-              });
-            }, 2000);
-            return;
-          }
-          else {
-            successAlert("Redirecting to select admin location");
-            setTimeout(() => {
-              navigate("/selectLoc", {
-                state: { adminTeacher: true, selectSchool: false, fromProfile: false }
-              });
-            }, 2000);
-            return;
-          }
-        }
+        // else if (userLocations.length >= 1) {
+        //   successAlert("Redirecting to select school location");
+        //   setTimeout(() => {
+        //     navigate("/selectLoc", {
+        //       state: { adminTeacher: false, selectSchool: true, fromProfile: false }
+        //     });
+        //   }, 2000);
+        //   return;
+        // }
+        // else if (userLocations.length === 0 && pendingLocations.length==0) {
+        //   if (user.adminTeacher && (user.role === 'Stanford Staff' || user.role === "Site Admin")) {
+        //     successAlert("Redirecting to select school location");
+        //     setTimeout(() => {
+        //       navigate("/selectLoc", {
+        //         state: { adminTeacher: false, selectSchool: true, fromProfile: false }
+        //       });
+        //     }, 2000);
+        //     return;
+        //   }
+        //   else {
+        //     successAlert("Redirecting to select admin location");
+        //     setTimeout(() => {
+        //       navigate("/selectLoc", {
+        //         state: { adminTeacher: true, selectSchool: false, fromProfile: false }
+        //       });
+        //     }, 2000);
+        //     return;
+        //   }
+        // }
       }
       else if (user && !user.adminTeacher && userLocations.length < 1) {
         console.log(userLocations)
