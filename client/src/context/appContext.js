1970c74b50dcbd113a12589881febc27a41e9703
cyriljoby <cyriljoby@gmail.com>
2024-09-10 17:59:51 -0700

    guam schools

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -50,13 +50,14 @@ const LSUser = JSON.parse(localStorage.getItem("user"));
 const LSUserLocations = JSON.parse(localStorage.getItem("userLocations"));
 const stateList = ["all", "Alabama", "Alaska", "Arizona", "Arkansas", "California",
       "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida",
-      "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
+      "Guam","Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
       "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
       "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada",
       "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina",
       "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
       "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
       "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+
 
 const initialState = {
   userLoading: false,
